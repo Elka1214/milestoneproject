a10e21d37e375e25ffd23d5bb5ca260120619945
Elka1214 <116694312+Elka1214@users.noreply.github.com>
2023-02-23 21:00:50 -0500

    Added images

---
+++ index.js
@@ -0,0 +1,50 @@
+const cardArray = [
+  {
+    name: "duck",
+    img: "images/duck.png",
+  },
+  {
+    name: "duck",
+    img: "images/duck.png",
+  },
+  {
+    name: "chick",
+    img: "images/chick.png",
+  },
+  {
+    name: "chick",
+    img: "images/chick.png",
+  },
+  {
+    name: "rooster",
+    img: "images/rooster.png",
+  },
+  {
+    name: "rooster",
+    img: "images/rooster.png",
+  },
+  {
+    name: "duck2",
+    img: "images/duck2.png",
+  },
+  {
+    name: "duck2",
+    img: "images/duck2.png",
+  },
+  {
+    name: "bunny",
+    img: "images/bunny.png",
+  },
+  {
+    name: "bunny",
+    img: "images/bunny.png",
+  },
+  {
+    name: "pig",
+    img: "images/pig.png",
+  },
+  {
+    name: "pig",
+    img: "images/pig.png",
+  },
+];
