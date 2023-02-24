1f37ab4f5415e217da57b55bcb4df2bc3692fb10
Elka1214 <116694312+Elka1214@users.noreply.github.com>
2023-02-24 16:47:29 -0500

    updated styling

--- index.js
+++ index.js
@@ -1,50 +1,52 @@
-const cardArray = [
-  {
-    name: "duck",
-    img: "images/duck.png",
-  },
-  {
-    name: "duck",
-    img: "images/duck.png",
-  },
-  {
-    name: "chick",
-    img: "images/chick.png",
-  },
-  {
-    name: "chick",
-    img: "images/chick.png",
-  },
-  {
-    name: "rooster",
-    img: "images/rooster.png",
-  },
-  {
-    name: "rooster",
-    img: "images/rooster.png",
-  },
-  {
-    name: "duck2",
-    img: "images/duck2.png",
-  },
-  {
-    name: "duck2",
-    img: "images/duck2.png",
-  },
-  {
-    name: "bunny",
-    img: "images/bunny.png",
-  },
-  {
-    name: "bunny",
-    img: "images/bunny.png",
-  },
-  {
-    name: "pig",
-    img: "images/pig.png",
-  },
-  {
-    name: "pig",
-    img: "images/pig.png",
-  },
-];
+document.addEventListener("DOMContentLoaded", () => {
+  const cardArray = [
+    {
+      name: "duck",
+      img: "images/duck.png",
+    },
+    {
+      name: "duck",
+      img: "images/duck.png",
+    },
+    {
+      name: "chick",
+      img: "images/chick.png",
+    },
+    {
+      name: "chick",
+      img: "images/chick.png",
+    },
+    {
+      name: "rooster",
+      img: "images/rooster.png",
+    },
+    {
+      name: "rooster",
+      img: "images/rooster.png",
+    },
+    {
+      name: "duck2",
+      img: "images/duck2.png",
+    },
+    {
+      name: "duck2",
+      img: "images/duck2.png",
+    },
+    {
+      name: "bunny",
+      img: "images/bunny.png",
+    },
+    {
+      name: "bunny",
+      img: "images/bunny.png",
+    },
+    {
+      name: "pig",
+      img: "images/pig.png",
+    },
+    {
+      name: "pig",
+      img: "images/pig.png",
+    },
+  ];
+});
